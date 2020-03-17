ccfeb146913810159ba66127930334e76abc406c
Théo Friberg <fsmnarmosta@posteo.net>
2020-03-17 14:26:05 +0200

    Fix login redirection

--- application/__init__.py
+++ application/__init__.py
@@ -29,7 +29,7 @@ from flask_login import LoginManager
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-login_manager.login_view = "auth_login"
+login_manager.login_view = "login"
 login_manager.login_message = "Please login to use this functionality."
 
 @login_manager.user_loader
