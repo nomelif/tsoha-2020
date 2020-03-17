a383970445b996424421cc57b4588c45e6abeee5
Théo Friberg <fsmnarmosta@posteo.net>
2020-03-17 13:04:12 +0200

    Added error handling for login and signup

--- application/views.py
+++ application/views.py
@@ -3,6 +3,7 @@ from flask import render_template, request, redirect, url_for
 from application.varkki.models import Account
 from flask_login import login_required, current_user, login_user, logout_user
 import bcrypt
+import sqlalchemy
 
 @app.route("/")
 def index():
@@ -29,17 +30,26 @@ def login():
     account_name = request.form.get("account")
     password = request.form.get("password")
     if request.form.get("create account") != None:
-        account = Account(account_name, password)
-        db.session().add(account)
-        db.session().commit()
-        login_user(account)
-        return redirect(url_for("index"))
-    else:
-        account = Account.query.filter_by(user_name=request.form.get("account")).first()
-        if account == None or password == None:
-            return render_template("login.html", title="Kirjaudu sisään")
-        elif bcrypt.checkpw(password.encode("utf-8"), account.password_hash.encode("utf-8")):
+        if account_name == None or password == None:
+            return render_template("login.html", title="Kirjaudu Värkkiin", error_message="Käyttäjätunnuksen ja salasanan on saatava arvo.", hide_login=True)
+        if len(password) < 8:
+            return render_template("login.html", title="Kirjaudu Värkkiin", error_message="Salasanan kuuluu olla vähintään kahdeksan merkin pituinen.", hide_login=True)
+        if len(account_name) == 0:
+            return render_template("login.html", title="Kirjaudu Värkkiin", error_message="Käyttäjätunnus ei saa olla tyhjä.", hide_login = True)
+        if len(account_name) > 20:
+            return render_template("login.html", title="Kirjaudu Värkkiin", error_message="Käyttäjätunnukselle on 20 merkin pituusraja.", hide_login=True)
+        try:
+            account = Account(account_name, password)
+            db.session().add(account)
+            db.session().commit()
             login_user(account)
             return redirect(url_for("index"))
+        except sqlalchemy.exc.IntegrityError:
+            return render_template("login.html", title="Kirjaudu Värkkiin", error_message="Käyttäjätunnus on jo käytössä.", hide_login=True)
+    else:
+        account = Account.query.filter_by(user_name=request.form.get("account")).first()
+        if account == None or password == None or not bcrypt.checkpw(password.encode("utf-8"), account.password_hash.encode("utf-8")):
+            return render_template("login.html", title="Kirjaudu sisään", error_message="Käyttäjätunnus ja salasana eivät täsmää.", hide_signup=True)
         else:
-            return render_template("login.html", title="Kirjaudu Värkkiin")
+            login_user(account)
+            return redirect(url_for("index"))
