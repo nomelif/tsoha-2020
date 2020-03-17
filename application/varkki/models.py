883daa4c208842cc03b9019e4d0c75c99ad3ce3b
Théo Friberg <fsmnarmosta@posteo.net>
2020-03-17 12:07:27 +0200

    Fix overly long password when postgres converts it itself

--- application/varkki/models.py
+++ application/varkki/models.py
@@ -8,7 +8,15 @@ class Account(db.Model):
 
     def __init__(self, user_name, password):
         self.user_name = user_name
-        self.password_hash = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+        # True for whatever was the current version on 17.3.2020:
+        # It is known from bcrypt source that hashpw returns a string containing chars:
+        # A-z0-9 and the litteral characters . / and $
+        # IE. b64 with $ added and + replaced by . for some reason.
+        # These are all ASCII characters and that guarantees that interpreting them as utf-8
+        # will not split any bytes into two. We can store the result as a utf-8 varchar(60).
+
+        self.password_hash = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
     def get_id(self):
         return self.id
