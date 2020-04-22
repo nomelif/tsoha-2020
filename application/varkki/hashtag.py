78001b2c34683599f12e83d03e178fc16ae83241
Théo Friberg <fsmnarmosta@posteo.net>
2020-04-22 16:07:41 +0300

    Mark hashtag text as unique

--- application/varkki/hashtag.py
+++ application/varkki/hashtag.py
@@ -3,7 +3,7 @@ from application import db
 class Hashtag(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(20), nullable=False)
+    text = db.Column(db.String(140), nullable=False, unique=True)
 
     def __init__(self, text):
         seld.text = text
